590c268dab7bb2db9849a8bdba166850a931d18d
hon121215 <120718733+hon121215@users.noreply.github.com>
2024-09-26 01:25:06 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -0,0 +1,13 @@
+def solution(dot):
+    answer = 0
+    if dot[0] > 0:
+        if dot[1] > 0:
+            return 1
+        elif dot[1] < 0:
+            return 4
+    elif dot[0] < 0:
+        if dot[1] > 0:
+            return 2
+        elif dot[1] < 0:
+            return 3
+    
